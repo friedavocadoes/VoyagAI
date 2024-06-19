36082cc0372a794ee4431a3dfc15c746e73fc2a8
gautham <desertncamel@gmail.com>
2024-06-19 11:17:14 +0530

    v4

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const run = require("./pre.js");
-const prompt = require('prompt-sync')();
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
@@ -20,11 +19,11 @@ app.post('/search', async (req, res) => {
         const mood = req.body.mood;
         const budget = req.body.budget;
         console.log(req.body.mood);
-        
         const text = await run(mood, budget);
         const jtext = JSON.parse(text);
-        console.log(jtext.vacation_spots[0].name);
-        res.send(jtext);
+        // console.log(jtext.vacation_spots[0].name);
+        // res.send(jtext);
+        res.render('forg', {vacation: jtext.vacation_spots});
     } catch {
         res.send("There was a problem in parsing the response, please try again");
     }
