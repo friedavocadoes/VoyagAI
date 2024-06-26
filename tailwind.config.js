6e929421153a8df5c7fe12d18ef5e7ff3a07ae7c
LUTHFI NASSIR <120134408+LuthfiNassir@users.noreply.github.com>
2024-06-27 01:34:21 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
