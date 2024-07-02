88c324e58a2e49351fcf11a8871bee4a5aad73b5
gautham <desertncamel@gmail.com>
2024-07-02 15:11:05 +0530

    bg image

--- api/pre.js
+++ api/pre.js
@@ -8,10 +8,10 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 
 async function run(mood, budget, loc) {
-    const prompt = `I want you to suggest me 6 vacation spots (around the world) based on my mood and budget.
+    const prompt = `I want you to suggest me 12 vacation spots (around the world) based on my mood and budget.
     My mood: ${mood}, budget (in indian rupees): ${budget}, my current location: ${loc}.
     I want the response to be a json with the names of the places, a brief description about each and a hyperlink to a picture of that place from the internet with the key "pict".
-    This is how i want the structure of the json to be: {"vacation_spots":[{"name":<name of the place>, "description":<the description>, "pict":<picture link>}, {"name":<name of the place>, "description":<the description>, "pict":<picture link>}, <and 4 more>]}.
+    This is how i want the structure of the json to be: {"vacation_spots":[{"name":<name of the place>, "description":<the description>, "pict":<picture link>}, {"name":<name of the place>, "description":<the description>, "pict":<picture link>}, <and 10 more>]}.
     There shud be no other text or formatting other than the json output, especially the json formatter text ('''json'''). the output should be plain string`;
 
     const result = await model.generateContent(prompt);
--- src/pages/Generator.jsx
+++ src/pages/Generator.jsx
@@ -16,7 +16,7 @@ const Generator = () => {
       <div className="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0">
 
         {/* the below line to see a demo logo */}
-        <div className="relative sm:w-1/2 xl:w-3/5 bg-blue-500 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden text-white bg-no-repeat bg-cover relative" style={{ backgroundImage: `url(https://as1.ftcdn.net/v2/jpg/02/05/26/62/1000_F_205266292_vsbwsjJwUiYMMPWLhW9DufnSndGU39Yo.jpg)` }}>
+        <div className="relative sm:w-1/2 xl:w-3/5 bg-blue-500 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden text-white bg-no-repeat bg-cover relative" style={{ backgroundImage: `url(src/assets/download.jpeg)` }}>
           <div className="absolute bg-black opacity-25 inset-0 z-0"></div>
           <div className="w-full lg:max-w-2xl md:max-w-md z-10 items-center text-center">
             <div className="font-bold leading-tight mb-6 mx-auto w-full content-center items-center text-slate-500	">
