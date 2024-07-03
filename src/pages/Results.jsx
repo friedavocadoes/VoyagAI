4916f7c4457a858602f11bc97b7e26704d43df2b
gautham <desertncamel@gmail.com>
2024-07-03 12:17:57 +0530

    design changes

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -28,11 +28,12 @@ const Results = () => {
 
   return (
     <>
+    <div className="mt-[120px] mx-20">
       {results ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5 m-4">
           {results.map((result, index) => (
             <div key={index} className="bg-orange-300 rounded-lg shadow-md overflow-hidden">
-              <img className="w-full h-48 object-cover" src={result.pict} alt={result.name} />
+              {/* <img className="w-full h-48 object-cover" src={result.pict} alt={result.name} /> */}
               <div className="p-4">
                 <h5 className="text-xl font-bold">{result.name}</h5>
                 <p className="text-gray-700 text-sm mb-2">{result.description}</p>
@@ -40,9 +41,17 @@ const Results = () => {
             </div>
           ))}
         </div>
+        
       ) : (
-        <p>Loading results...</p>
+        <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
+          <div class="p-4 bg-gradient-to-tr animate-spin from-green-500 to-blue-500 via-purple-500 rounded-full">
+            <div class="bg-white rounded-full">
+            <div class="w-24 h-24 rounded-full"></div>
+            </div>
+          </div>
+        </div>
       )}
+      </div>
     </>
   );
 };
