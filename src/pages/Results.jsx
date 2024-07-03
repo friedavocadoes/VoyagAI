c99b9f8cf8d8b1383d4154ace79cd47aa8f8a2da
gautham <desertncamel@gmail.com>
2024-07-03 16:55:23 +0530

    spinner

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -45,7 +45,7 @@ const Results = () => {
       ) : (
         <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2">
           <div class="p-4 bg-gradient-to-tr animate-spin from-green-500 to-blue-500 via-purple-500 rounded-full">
-            <div class="bg-white rounded-full">
+            <div class="bg-opacity-0 rounded-full">
             <div class="w-24 h-24 rounded-full"></div>
             </div>
           </div>
