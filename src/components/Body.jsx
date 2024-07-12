2b97c691cd5381580373b2aa9a45ecb550bd3185
LuthfiNassir <connectluthfi@gmail.com>
2024-07-12 21:41:39 +0400

    redesigned pages

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,11 +5,11 @@ const Layout = ({ children }) => {
     <div className="relative px-6 flex items-center justify-center"> {/* Removed min-h-screen for flexibility */}
       {/* Gradient background (combined and simplified) */}
       <div
-        className="absolute inset-x-0 top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        className="fixed inset-x-0 top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(75%-30rem)]"
         aria-hidden="true"
       >
         <div
-          className="relative w-full aspect-[16/5] bg-gradient-to-tr from-[#fda7cb] to-[#9089fc] opacity-20 sm:h-[calc(100%+30rem)]"
+          className="relative w-full aspect-[16/5] bg-gradient-to-tr from-[#fda7cb] to-[#9089fc] opacity-20 md:h-[calc(100%+10rem)]"
         />
       </div>
       <div className="layout-content"> {/* More descriptive class name */}
