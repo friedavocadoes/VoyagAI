d805b53f77c93f3dc46ab9c6b03ff3ebfdd3b72e
gautham <desertncamel@gmail.com>
2024-07-13 12:17:47 +0530

    favicon

--- src/pages/Generator.jsx
+++ src/pages/Generator.jsx
@@ -113,8 +113,8 @@ const Generator = () => {
                 {...register('mood')}
                 placeholder="How are you feeling today? (or use the slider below)"
                 className="border border-gray-600 bg-gray-700 text-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4"
-                value={moodDescriptions[moodValue]}
-                readOnly
+                value={moodDescriptions[moodValue] && moodDescriptions[moodValue]}
+                
               />
               <input
                 type="range"
