d6137d6b2be267df5b6cf83af2e30343b050a13c
gautham <desertncamel@gmail.com>
2024-07-13 08:30:10 +0530

    9 results

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,17 +5,17 @@ const Homepage = () => {
   return (
     <div className="relative px-6 pt-[250px] flex items-center justify-center min-h-screen">
       <div className="text-center">
-      <h1 class="text-4xl font-bold tracking-tight text-[#767fb4] sm:text-8xl">Your <span class="text-[#f9b17a] ">SMART </span> Travel Planner</h1>
-      <p class="mt-6 text-xl leading-8 text-white ">Explore the world with ease and embark on your next adventure.</p>
-      <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="/generator" class="rounded-md bg-[#f9b17a] px-3.5 py-2.5 text-lg font-bold text-[#2d3250] shadow-sm hover:bg-[#767fb4] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Start planning with AI</a>
+      <h1 className="text-4xl font-bold tracking-tight text-[#767fb4] sm:text-8xl">Your <span className="text-[#f9b17a] ">SMART </span> Travel Planner</h1>
+      <p className="mt-6 text-xl leading-8 text-white ">Explore the world with ease and embark on your next adventure.</p>
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <a href="/generator" className="rounded-md bg-[#f9b17a] px-3.5 py-2.5 text-lg font-bold text-[#2d3250] shadow-sm hover:bg-[#767fb4] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Start planning with AI</a>
       </div>
       <img
         alt="VoyagAI"
         src="/scene.png"
         className="object-cover mx-auto pt-15"
       />
-      <h1 class="text-3xl font-bold tracking-tight text-[#767fb4] sm:text-6xl pb-20 pt-10">Your <span class="text-[#f9b17a] ">AI </span> Power Trip</h1>
+      <h1 className="text-3xl font-bold tracking-tight text-[#767fb4] sm:text-6xl pb-20 pt-10">Your <span className="text-[#f9b17a] ">AI </span> Power Trip</h1>
             <div className="flex flex-wrap justify-center items-center px-4 py-2 space-y-8 pb-10">
         <section className="w-full flex flex-col space-y-4 md:w-1/2">  {/* Adjust width on medium screens */}
           <h2 className="text-4xl font-bold text-white">The Most Optimal</h2>
