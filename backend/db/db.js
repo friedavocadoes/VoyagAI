0009f31dadce70acf9f8202f984f03b8c671b1a2
gautham <desertncamel@gmail.com>
2024-07-16 23:02:12 +0530

    mongo uri

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,12 @@
 
 import mongoose from 'mongoose';
 
+
+
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/auth', {
+    console.log(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
     });
     console.log('MongoDB connected');
   } catch (err) {
