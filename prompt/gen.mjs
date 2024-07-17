4fbf328dacadfacd07d2febeaefc58e0c7bdf403
gautham <desertncamel@gmail.com>
2024-07-17 19:15:35 +0530

    Update gen.mjs

--- prompt/gen.mjs
+++ prompt/gen.mjs
@@ -10,6 +10,7 @@ async function run(mood, budget, loc) {
     const prompt = `I want you to suggest me 9 vacation spots (around the world) based on my mood and budget.
     My mood: ${mood}, budget (in indian rupees): ${budget}, my current location: ${loc}.
     I want the response to be a json with the names of the places, a description about each, ways to travel to the location, stay options (hotels and resorts) at the location, a budget plan and a hyperlink to a website about that place from the internet (make sure the link is working currently).
+    Also in the description, I need to know how that place is right for my mood and its significance.
     This is how i want the structure of the json to be: {"vacation_spots":[{"name":<name of the place>, "description":<the description>, "travel":<travel options>, "stay":<hotels or resort options>, "budget":<avg budget in INR>, "link":<website link>}, <and 8 more>]}.
     There shud be no other text or formatting other than the json output, especially the json formatter text ('''json''') shouldnt be there. the output should be plain string`;
 
