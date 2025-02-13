6f1df4a09e28e26795da5b703a5b85cb3e0babec
gautham <desertncamel@gmail.com>
2025-02-13 20:44:44 +0530

    results

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
-import axios from 'axios';
-import Loader from '../components/Loader';
-import { getEmailFromToken } from '../utils/auth';
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import Loader from "../components/Loader";
+import { getEmailFromToken } from "../utils/auth";
 
 const Results = () => {
   const location = useLocation();
@@ -12,26 +12,27 @@ const Results = () => {
 
   const colorToggle = async (result, index) => {
     let fav = document.getElementById(`wish${index}`);
-    fav.classList.toggle('fill-red-500');
+    fav.classList.toggle("fill-red-500");
 
-
-    if (fav.classList.contains('fill-red-500')) {
+    if (fav.classList.contains("fill-red-500")) {
       console.log(result);
 
       const email = getEmailFromToken();
 
       try {
-        const response = await axios.post('https://voyagai.onrender.com/wishlist/save', { email, result }, {
-          headers: { 'Content-Type': 'application/json' },
-        });
-        
+        const response = await axios.post(
+          "https://voyagai.onrender.com/wishlist/save",
+          { email, result },
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
       } catch (error) {
-        console.error('Error fetching results:', error);
+        console.error("Error fetching results:", error);
       }
     }
-
   };
-  
+
   useEffect(() => {
     window.scrollTo(0, 0);
 
@@ -39,19 +40,28 @@ const Results = () => {
       setIsLoading(true);
       if (data) {
         try {
-          const response = await axios.post('https://voyagai.onrender.com/search', data, {
-            headers: { 'Content-Type': 'application/json' },
-          });
+          const response = await axios.post(
+            "http://localhost:5000/search",
+            data,
+            {
+              headers: { "Content-Type": "application/json" },
+            }
+          );
           setResults(response.data.vacation_spots);
         } catch (error) {
-          console.error('Error fetching results:', error);
-          setResults([{ name: "API Error", description: "There was an error fetching your request. Please try again." }]);
+          console.error("Error fetching results:", error);
+          setResults([
+            {
+              name: "API Error",
+              description:
+                "There was an error fetching your request. Please try again.",
+            },
+          ]);
         } finally {
           setIsLoading(false);
         }
       }
     };
-      
 
     fetchData();
   }, [data]);
@@ -77,61 +87,79 @@ const Results = () => {
               {results ? (
                 results.map((result, index) => (
                   <div
-                key={index}
-                className={`bg-slate-300 bg-opacity-50 hover:bg-opacity-90 backdrop-blur-[12px] rounded-md shadow-md overflow-hidden m-2 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl hover:bg-slate-100`}
-            >
-                {/* <img className="w-full h-48 object-cover" src={result.pict} alt={result.name} /> */}
-                <div className="p-5 pb-3">
-                    <a href={result.link} target="_blank">
-                        <h5 className="text-xl font-bold mb-3 text-slate-900">{result.name}</h5>
+                    key={index}
+                    className={`bg-slate-300 bg-opacity-50 hover:bg-opacity-90 backdrop-blur-[12px] rounded-md shadow-md overflow-hidden m-2 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl hover:bg-slate-100`}
+                  >
+                    {/* <img className="w-full h-48 object-cover" src={result.pict} alt={result.name} /> */}
+                    <div className="p-5 pb-3">
+                      <a href={result.link} target="_blank">
+                        <h5 className="text-xl font-bold mb-3 text-slate-900">
+                          {result.name}
+                        </h5>
                         <p className="text-gray-700 text-sm mb-2">
-                        <span className="text-slate-800 font-bold">Description: </span>
-                        {result.description}
+                          <span className="text-slate-800 font-bold">
+                            Description:{" "}
+                          </span>
+                          {result.description}
                         </p>
                         {result.travel ? (
-                        <p className="text-gray-700 text-sm mb-2">
-                            <span className="text-slate-800 font-bold">Travel: </span>
+                          <p className="text-gray-700 text-sm mb-2">
+                            <span className="text-slate-800 font-bold">
+                              Travel:{" "}
+                            </span>
                             {result.travel}
-                        </p>
+                          </p>
                         ) : (
-                        <> </>
+                          <> </>
                         )}
                         {result.stay ? (
-                        <p className="text-gray-700 text-sm mb-2">
-                            <span className="text-slate-800 font-bold">Stay Options: </span>
+                          <p className="text-gray-700 text-sm mb-2">
+                            <span className="text-slate-800 font-bold">
+                              Stay Options:{" "}
+                            </span>
                             {result.stay}
-                        </p>
+                          </p>
                         ) : (
-                        <> </>
+                          <> </>
                         )}
                         {result.budget ? (
-                        <>
+                          <>
                             <p className="text-gray-700 text-sm mb-2">
-                            <span className="text-slate-800 font-bold">Budget (est.): </span>
-                            {result.budget}
+                              <span className="text-slate-800 font-bold">
+                                Budget (est.):{" "}
+                              </span>
+                              {result.budget}
                             </p>
-                        </>
+                          </>
                         ) : (
-                        <> </>
+                          <> </>
                         )}
-                    </a>
-                    <hr className="border-gray-300" />
-                    <div className="mt-2 cursor-pointer">
-                        <svg id={`wish${index}`}
-                        onClick={() => colorToggle(result, index)}
-                        className="h-5 w-5 text-gray-800 hover:text-red-500 hover:fill-red-500 transition duration-100 ease-in-out" 
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
+                      </a>
+                      <hr className="border-gray-300" />
+                      <div className="mt-2 cursor-pointer">
+                        <svg
+                          id={`wish${index}`}
+                          onClick={() => colorToggle(result, index)}
+                          className="h-5 w-5 text-gray-800 hover:text-red-500 hover:fill-red-500 transition duration-100 ease-in-out"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                          />
                         </svg>
-                        
+                      </div>
                     </div>
-                </div>
-            </div>
+                  </div>
                 ))
               ) : (
-                <p className="text-xl font-semibold text-slate-800">No results found.</p>
+                <p className="text-xl font-semibold text-slate-800">
+                  No results found.
+                </p>
               )}
             </div>
           </>
